b39cf50382fd124eb80984ec40932e3bc12e1074
smsadat1 <smsadat788@gmail.com>
2026-04-09 18:49:49 +0600

    chore: Setup language specific images

---
+++ app/docker/languages/Dockerfile.js
@@ -0,0 +1,5 @@
+# borealis-exec-js
+FROM node:20-slim
+WORKDIR /workspace
+USER 1000:1000
+CMD ["sh"]
---
+++ app/docker/languages/Dockerfile.py
@@ -0,0 +1,5 @@
+# borealis-exec-py
+FROM python:3.12-slim
+WORKDIR /workspace
+USER 1000:1000
+CMD ["sh"]
